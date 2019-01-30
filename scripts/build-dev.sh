71801f9da526ea7d19aad0afd31cf0c6fd6698f3
Ian O´Brian <Ian.OBrian@husbanken.no>
2019-01-30 13:06:19 +0100

    .gitignore is now working

--- scripts/build-dev.sh
+++ scripts/build-dev.sh
@@ -1,9 +1,6 @@
 #!/usr/bin/env bash
 
 mvn -T 2C clean install -DskipTests
-#pushd frontend
-#npm install && npm run build-no-aot
-#popd
 
-docker-compose -f ./docker/docker-compose-dev.yml build
-docker-compose -f ./docker/docker-compose-dev.yml -p penger up --abort-on-container-exit
+docker-compose -f docker-compose-dev.yml build
+docker-compose -f docker-compose-dev.yml -p penger up --abort-on-container-exit
